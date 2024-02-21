14817cd39ddae853c23480deb8faa6133273ed47
Danyeel12 <Miranda.D1103@gmail.com>
2024-02-20 21:34:08 -0500

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ import { services } from "../constants";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
 import profilePic from "../assets/me.png";
-import resumeFile from '../assets//ResumeDanielMiranda.pdf';
+import resumeFile from '../assets//Resume_DanielMiranda.pdf';
 
 
 const ServiceCard = ({ index, title, icon }) => (
